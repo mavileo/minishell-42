9b02a03276f0c320ab79daabf11148b48bee2828
pmouhali <p.m.272019@gmail.com>
2020-07-07 18:58:03 +0200

    get_next_token sans bonus + squelette command_container

--- tmp.c
+++ tmp.c
@@ -18,11 +18,9 @@ int		main(int ac, char *av[], char *envp[])
 	get_next_line(0, &input);
 
 	list = input_to_token_list(input, NULL);
-
-	t_token *token = list->content;
-	get_abs_value(token->args);
-
 	ft_print_token_list(list);
+
+	get_next_token(list);
 
 	free(input);
 	ft_lstclear(&list, (void*)ft_token_free);
