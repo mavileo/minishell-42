be328d80272244415491476d898de59b38889144
pmouhali <p.m.272019@gmail.com>
2020-07-24 16:34:29 +0200

    empty arguments : ok

--- libft/ft_tabpush.c
+++ libft/ft_tabpush.c
@@ -1,4 +1,14 @@
-// 42 HEADER
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabpush.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/24 16:32:41 by user42            #+#    #+#             */
+/*   Updated: 2020/07/24 16:33:38 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -10,7 +20,7 @@
 **	creates a copy of the given two-dimensional array with the given string
 **	to push added at the end of the array.
 **
-**	if the given string to push if null or empty, nothing is done
+**	if the given string to push if null, nothing is done
 **
 **	this function frees the given two-dimensional array, so it needs to be
 **	previously allocated.
@@ -38,7 +48,7 @@ char	**ft_tabpush(char *tab[], const char *s)
 	int len;
 	int i;
 
-	if (!s || s[0] == 0)
+	if (!s)
 		return (tab);
 	len = tab ? ft_tablen((const char **)tab) + 1 : 1;
 	if (!(copy = (char**)malloc(sizeof(*tab) * (len + 1))))
